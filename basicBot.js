8f888a09cd5daa96136b1ba4505dc7877ecd5300
Depys <33812707+Depys@users.noreply.github.com>
2018-03-02 21:58:17 -0300

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2013,7 +2013,7 @@
 
             clearchatCommand: {
                 command: 'clearchat',
-                rank: 'dj',
+                rank: 'bouncer',
                 type: 'exact',
                 functionality: function(chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
